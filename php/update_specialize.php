8bd5bc1f9888797ea48f8a628b92e9866bd047d9
AbdulnaserMohsen <naserahmed1995@gmail.com>
2020-12-21 23:59:46 +0200

    4commit

--- php/update_specialize.php
+++ php/update_specialize.php
@@ -10,24 +10,34 @@
 	$book_en = $_POST['book_en'];
 	
 	$image = $_FILES['image'];
-	require_once('initialize.php');
-	$image_path = "images/";
-	$image_name= strtotime(1). basename($image["name"]);
-	$target_file = "../public/images/".$image_name;
-	
-	if (!move_uploaded_file($image["tmp_name"], $target_file)) 
+		
+	if(!empty($image["name"]))
 	{
-		return ( "Sorry, there was an error uploading your file.");
+		require_once('initialize.php');
+		$image_path = "images/";
+		$image_name= strtotime(1). basename($image["name"]);
+		$target_file = "../public/images/".$image_name;
+		if (!move_uploaded_file($image["tmp_name"], $target_file)) 
+		{
+			return ( "Sorry, there was an error uploading your file.");
+		}
+		$image = $image_path.$image_name;	
 	}
-	$image = $image_path.$image_name;	
+	else
+	{
+		echo "no image";
+		$image = "";
+	}
+
 	//echo $name_ar,$name_en,$description_ar,$description_en,$alt,$book_ar,$book_en."<br	>";
 	//var_dump($image);
 	
+	require_once('initialize.php');
 	require_once('specializes.php');
 	$result = update_specialize($id,$name_ar,$name_en,$description_ar,$description_en,$alt,$book_ar,$book_en,$image);
 	if($result == "SUCCESS")
 	{
-		redirect_to	('admin/admin-specialize.php');
+		redirect_to('admin/admin-specialize.php');
 	}
 	else
 	{
